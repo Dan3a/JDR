36d182dae93a6150b5320e6d746c6a375f2d0007
rremi0 <91263959+rremi0@users.noreply.github.com>
2021-10-07 15:32:43 +0200

    2

--- #exo 6.py
+++ #exo 6.py
@@ -3,7 +3,7 @@ from random import *
 n=1
 d=0
 e=0
-while n<5001:
+while n<1001:
     a=randint(1,8)
     b=randint(1,8)
     print(a,b,end=" ")
@@ -12,4 +12,7 @@ while n<5001:
     elif a+b==12:
         e=e+1
     n=n+1
+x=d*100/1000
+y=e*100/1000
 print("le total 2 est sorti : ",d," fois et le total 12 est sorti : ",e," fois")
+print("il y a : ",x,"pourcent de chance d'avoir 2 et : ",y,"pourcent de chance d'avoir 12")
