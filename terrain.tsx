d3a57a5033fb5824b347c54b58ef8ff043d9a37a
rremi0 <91263959+rremi0@users.noreply.github.com>
2021-10-19 09:29:27 +0200

    map

---
+++ terrain.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="terrain" tilewidth="16" tileheight="16" tilecount="528" columns="12">
+ <image source="terrain/tileset_basic_terrain.png" trans="000000" width="192" height="704"/>
+</tileset>
