ef7017df4320b5f05f2c4fe1dbca65f2e168e508
dan3a <21985756+Dan3a@users.noreply.github.com>
2021-12-09 21:55:10 +0100

    Ajout de la petite icône en haut à droite (qui est très sexy)

--- startmenu.py
+++ startmenu.py
@@ -1,5 +1,45 @@
-from tkinter import *
+# Image en base64
+iconimgdata = b'iVBORw0KGgoAAAANSUhEUgAAABcAAAAbCAYAAACX6BTbAAAGFElEQVRIiWVWS' \
+              b'4gc1xU9971XVV3V1b/pVqtHI9mjsUbIRDKGxFmZBBKTRRZJcIwXgWyyTggEvM' \
+              b'w6i6xs8MLbEAwhCs4i5IMhiBCMkBkTy8lYsjUfW5rpmemZ/lV3dVXX55n7qmc' \
+              b'kOw8eXVVdde+pc889t+h3G0dIcw1e7bKFy0slVEsSkziDZ0v4jsD2SazvPAzw' \
+              b'7bUq3tsN8OLlKiqOhC2JMg3sj+c4ChIIAqQgSAIsSVCDMEWlJE3w3jRB2QSUa' \
+              b'PsW/rU91q+9cQcizynpjvRbqy2KP+/r1wWRFNBrP7yh33z5Ck6mCZ9DEkEsfo' \
+              b'mPNw9DHAZzEIq1O4jg2UL/8e6x/sVrf6PkUZ8D4+L3r6N5YxmrP7gODqRI0N5' \
+              b'fNuknv3oX80xrm5HyFqcbUJ/0ZgiiDNNWjmeaJZSU0L/88xbeu3nXIHBqHmxL' \
+              b'mNe0zRbwWxWkg5ATIA9iuv1ZoF95rqkncV68gSBDkVo/52L7eIZRlOLW1kgPu' \
+              b'hMEG9sQRMQInJKEWy9zUpOAd7XlQyx5yCdzzAch7r6zieXq8/jmJV9rgNSCHn' \
+              b'G94+HFtZpBdLFqY743gCRBkoRBRlFWoFYERwk4/BaKINMcySCETQJ6ltD7f30' \
+              b'AIsCRQksJBHEG1ZskYPTPtl39VMPBz3eGmEtCPp7B9mzYtoIlBD8E2ypoiQRB' \
+              b'E1Au29AcRBDCvTHe+ccOfvS9y3g0jLUlBamP9qe4dxjq716t4zdvbyLc3IdSE' \
+              b'm6zjNa1DuKDERxJBjGj5+CcSFZLULUSRlt9MA1KCpp2p3ocZaawjiIofoC1ut' \
+              b'2PkJ8ERgVSa2QnU6SDKUqWMjcz50VwpkjAEmTo0Q0PQms0Oj78io2yI0/lqJU' \
+              b'SQtcciXpJwa24gKOQjyJYSsIGoBSZQI5FBTWLBJYEGu0yznfKiE5mcCxlrjHN' \
+              b'Os0w2ptAffpxD+VOBQfB3CC0yiVIz0G17cFWEukwgiWFQf5kcN6ep2ALAgVJ8' \
+              b'awgjPYCI4DoeAaV9gLE0Rza5htF8aBjoVwtwbEkZsH8cXBV/O8sZHmaLF1IlA' \
+              b't/WhfeKj0cY3ZEcC434TgW/KaLatvHhfOe8Y+PDyYGUcl6jJx/+Vql6kBBA54' \
+              b'FpLkBEZ9E3LHIwgRKCW4WoWudKvyGg6urdVOUcZTi6YaDrQW/BfKimIyKr609' \
+              b'VTXaDpsepkdThMcRkkmC/c2+6W4uqJFSchTga9/oQAjS3X5EaX+GScOB6yrIL' \
+              b'IdrPaaE0bslheWqbTq72XYhOh6GvRDhYI6jewO2ABIWd6IgyoYRPtse4pONLu' \
+              b'aHE9TPefj6io/Wsm/QMnJO4JrCEtorFZzzLUMPe4kthb64UsHxzrAwLo5rDti' \
+              b'AZwkO7jzCtZfW8MxqDb4lTZFqSy7So/DLyKXAUstFrjV8Rxn/hgDYWILDCEqw' \
+              b'fRCE8Q0y1SfiVtYaaca3aez2Y3RanqGBkZuiLiTZbrkYRVlhv5I0T42/v/URo' \
+              b'vHc8G2us/zOPJiIuu939dOrdYO6U7HNVOov+ybwqb5bF3xcqjvIcm2UwfZ66/' \
+              b'f3kYSZiVFYLiMXhfgtIiOldBzj89t7uNb2dN2VpmidCxWjlFOtL1+qoMZWbAk' \
+              b'seUpzoKAXQxGoGHPFqFMXb7QRdkMkY8MVTxDq3e/j4TDWz6/4PGHohasNxGmO' \
+              b'fpiaZN4517wpEemdrSFuvf0pBzM8NzseXnp1HY2mA/XTV59FnGncO5hif+MAO' \
+              b'k7hVxzTmUxJmOR6yVNIMo2yLcx8HUeZ5gF+87cfnPJL68818Z2XrxTFXSz6cH' \
+              b'+KlZptupHX3mhupjlb9oWqjX//8yEe3O7CDA9xOt1pMVCK85/9+gVD21eXejS' \
+              b'MjRqqJR4KwPmKxed6qxvi5h/+a9r6Wz9ep0vr9TP/4Ddi/jnZE0s/eZLOc1Ls' \
+              b'5Q8HMTeHZgXwJ8x//neCO396YHhllCe7Y33l2pIJzp8hLMWvBP7SevDhMYbHE' \
+              b'ejW1sj8wVP/g3d3H/sCf5sYPo1E2RbO9MuDgFAcCxAa7RKEFOgfhGdJpCVJ3d' \
+              b'84RNCd6OOd0UItHPT/+DWFOtMvQGa688cPgGEvepISQ36WZPgCVhY7eBY73j4' \
+              b'AAAAASUVORK5CYII='
 
+from tkinter import *
+import base64
+
+# Classe du menu principal
 class StartMenu(Frame):
     def __init__(self, root):
         Frame.__init__(self, root)
@@ -10,5 +50,7 @@ class StartMenu(Frame):
         canvas = Canvas(root, width = 800, height = 600)      
         canvas.pack()      
         self.img = PhotoImage(file="logolarge.gif")  # Use self.image
-        logo = canvas.create_image(400,0, anchor=N, image=self.img) 
-    
+        logo = canvas.create_image(400,0, anchor=N, image=self.img)
+        img = base64.b64decode(iconimgdata)
+        photo = PhotoImage(data=img)
+        root.iconphoto(False, photo)
